dd0d19f6bc6e7db3e4b8bb95a4baac80419f25a7
vharish31 <harishhvp31@gmail.com>
2026-02-01 13:33:42 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://lms-backend-34d2.onrender.com",
         changeOrigin: true,
       },
     },
